840daaf51600b32e8c00324d8058770ee67793b2
Jon Whittingham <jon@crownparkcomputing.com>
2025-05-13 16:29:58 +0100

    Release v1.8.7

--- lib/pages/test_import.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'game_library_page.dart';
-
-void main() {
-  // Just a test to verify the import works
-  GameLibraryPage? page;
-  print('Import test successful');
-}
